aa880c18163cafb3cab306768d0a91b5a6bad9e0
Naimur Rahaman <2015naim@gmail.com>
2021-02-11 23:23:35 +0600

    Meals show properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
 //Search Items
-const searchItem = () => {
-    const searchItem = document.getElementById('input-items').value;
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchItem}`;
+const searchMeal = () => {
+    const searchMeals = document.getElementById('input-meal').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeals}`;
     fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => displayMeals(data.meals));
 }
+
+const displayMeals = meals => {
+    const showMeals = document.getElementById('show-meals');
+    
+meals.forEach(meal => {
+    
+    const mealsContainer = document.createElement('div');
+    mealsContainer.className = 'meal';
+    
+    const mealsInfo = `
+    <img src="${meal.strMealThumb}">
+    <h2>${meal.strMeal}</h2>
+    `;
+    mealsContainer.innerHTML = mealsInfo;
+
+    showMeals.appendChild(mealsContainer);
+    });
+    
+    
+};
